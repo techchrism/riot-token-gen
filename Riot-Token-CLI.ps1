709d7e206790ceda9403d4006d06e628b54754da
techchrism <chris@techchrism.me>
2021-06-08 21:16:02 -0700

    initial working code

---
+++ Riot-Token-CLI.ps1
@@ -0,0 +1,18 @@
+. .\New-Riot-Token.ps1
+
+$username = Read-Host 'Enter Riot username'
+$password = Read-Host 'Enter Riot password' -AsSecureString
+
+$tokenData = New-Riot-Token -Username $username -Password $password
+
+Write-Host ''
+Write-Host 'Access Token:'
+Write-Host $tokenData.accessToken
+Write-Host ''
+
+Write-Host 'Entitlement:'
+Write-Host $tokenData.entitlement
+Write-Host ''
+
+Write-Host "User ID: $($tokenData.userid)"
+Write-Host "Expires in $($tokenData.expiration) seconds"
