2693428830abb87a37700462679b543957f8d3db
techchrism <chris@techchrism.me>
2021-06-09 11:18:11 -0700

    add hta building

--- build/index.js
+++ build/index.js
@@ -6,9 +6,10 @@ const path = require('path');
 {
     const files = [
         fs.readFile(path.join(__dirname, '..', 'New-Riot-Token.ps1'), 'utf8'),
-        fs.readFile(path.join(__dirname, '..', 'Riot-Token-CLI.ps1'), 'utf8')
+        fs.readFile(path.join(__dirname, '..', 'Riot-Token-CLI.ps1'), 'utf8'),
+        fs.readFile(path.join(__dirname, 'template.hta'), 'utf8'),
     ];
-    const [riotTokenFunction, riotTokenCLI] = await Promise.all(files);
+    const [riotTokenFunction, riotTokenCLI, htaTemplate] = await Promise.all(files);
 
     try
     {
@@ -17,5 +18,13 @@ const path = require('path');
     catch(ignored) {}
 
     const builtScriptText = riotTokenCLI.replace('. .\\New-Riot-Token.ps1', riotTokenFunction);
-    await fs.writeFile(path.join(__dirname, '..', 'dist', 'Riot-Token-CLI.ps1'), builtScriptText, 'utf8');
+    const scriptURL = 'https://techchrism.github.io/riot-token-gen/Riot-Token-CLI.ps1';
+    const htaText = htaTemplate.replace('{{encoded_data}}',
+        Buffer.from(` -Exe Bypass -C "& {((New-Object Net.WebClient).DownloadString('${scriptURL}')) | Invoke-Expression}"`).toString('base64'));
+
+    const outFiles = [
+        fs.writeFile(path.join(__dirname, '..', 'dist', 'Riot-Token-CLI.ps1'), builtScriptText, 'utf8'),
+        fs.writeFile(path.join(__dirname, '..', 'dist', 'run.hta'), htaText, 'utf8')
+    ];
+    await Promise.all(outFiles);
 })();
